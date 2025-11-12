fb5ba14dca20402996b8c09f5d1b94a9a488c771
Mateo_MID3B <pn41htu@eduvaud.ch>
2025-11-12 08:50:42 +0100

    continue ex Trigger

--- personnel/activite-serial/Serial/Serial/Program.cs
+++ personnel/activite-serial/Serial/Serial/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            
+            Character Gerard = new Character { FirstName = "Gérard", LastName = "Franco", Description = "Adore vraiment le miel"};
+            Character Maya = new Character { FirstName = "Maya", LastName = "Yémaya", Description = "Est encore plus fan du miel que Gérard" };
+
         }
+    }
+    public class Character
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public Actor PlayedBy { get; set; }
+    }
+
+    public class Actor
+    {
+
     }
 }
